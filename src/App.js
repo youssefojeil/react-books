9d2f76c09756eab5ff396b85f2f3892f2b8816ee
Youssef <youssef.ojeil@hotmail.com>
2023-04-14 20:03:17 -0400

    Book create component, handle change, submit and form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Create a book with title:", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BookCreate = () => {
-  return <div>BookCreate</div>;
+const BookCreate = ({ onCreate }) => {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" onChange={handleChange} value={title} />
+        <button className="button">Create</button>
+      </form>
+    </div>
+  );
 };
 
 export default BookCreate;
